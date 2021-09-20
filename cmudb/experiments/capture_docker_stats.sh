a2c89183e1534cd95c44fdbdc48c55319ac0556c
Wan Shen Lim <wanshen.lim@gmail.com>
2021-09-20 09:00:24 -0400

    Checkpoint.

--- cmudb/experiments/capture_docker_stats.sh
+++ cmudb/experiments/capture_docker_stats.sh
@@ -3,4 +3,4 @@
 CONTAINER_ID=$1
 FILENAME=$2
 
-echo -ne "GET /containers/$CONTAINER_ID/stats HTTP/1.1\r\nHost: woof\r\n\r\n" | sudo nc -U /var/run/docker.sock | grep "^{" > $FILENAME
+echo -ne "GET /containers/$CONTAINER_ID/stats HTTP/1.1\r\nHost: woof\r\n\r\n" | nc -U /var/run/docker.sock | grep "^{" > $FILENAME
