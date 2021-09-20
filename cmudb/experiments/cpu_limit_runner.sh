dd851282280c35710782d2dcb9186b961527c984
Wan Shen Lim <wanshen.lim@gmail.com>
2021-09-19 22:29:45 -0400

    Checkpoint.

--- cmudb/experiments/cpu_limit_runner.sh
+++ cmudb/experiments/cpu_limit_runner.sh
@@ -8,8 +8,8 @@ CPUS=( 0.5 0.25 0.2 0.15 0.1 0.09 0.08 0.07 0.06 0.05 0.04 0.03 0.02 0.01 )
 sudo echo "CPUS: ${CPUS[@]}"
 set -x
 
-docker build --tag pgnp --file ./cmudb/env/Dockerfile .
+# docker build --tag pgnp --file ./cmudb/env/Dockerfile .
 for cpu in "${CPUS[@]}"
 do
-  ./cmudb/experiments/cpu_limit.sh $cpu 2>&1 > ${cpu}_out.txt
+  ./cmudb/experiments/cpu_limit.sh $cpu 2>&1 > cpu_${cpu}_out.txt
 done
