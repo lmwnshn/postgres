b9cfac9b81bccf176f7228a9682108cc3394767a
Wan Shen Lim <wanshen.lim@gmail.com>
2021-09-19 21:46:27 -0400

    Add rebuild to runner.

--- cmudb/experiments/cpu_limit_runner.sh
+++ cmudb/experiments/cpu_limit_runner.sh
@@ -3,6 +3,8 @@
 set -e
 set -x
 
+sudo docker build --tag pgnp --file ./cmudb/env/Dockerfile .
+
 CPUS=( 0.5 0.25 0.2 0.15 0.1 0.09 0.08 0.07 0.06 0.05 0.04 0.03 0.02 0.01 )
 for cpu in "${CPUS[@]}"
 do
