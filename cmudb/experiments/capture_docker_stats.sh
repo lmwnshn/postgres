7b47d3a0386452121c194a4813d8b07a643e2ef5
Wan Shen Lim <wanshen.lim@gmail.com>
2021-09-19 21:23:24 -0400

    Add capture docker stats script.

---
+++ cmudb/experiments/capture_docker_stats.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+CONTAINER_ID=$1
+
+echo -ne "GET /containers/$CONTAINER_ID/stats HTTP/1.1\r\nHost: woof\r\n\r\n" | sudo nc -U /var/run/docker.sock | grep "^{" > docker_stats_$CONTAINER_ID.txt
