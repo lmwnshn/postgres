954f39c49aca8165fa05e19e10f8e9a274b5fc85
Wan Shen Lim <wanshen.lim@gmail.com>
2021-09-23 10:10:15 -0400

    Checkpoint.

--- cmudb/experiments/motivation.sh
+++ cmudb/experiments/motivation.sh
@@ -13,7 +13,7 @@ HOST_REPLICA="dev6.db.pdl.local.cmu.edu"
 USER_REPLICA="wanshenl"
 PGUSER_REPLICA="noisepage"
 PGPASS_REPLICA="terrier"
-PGPORT_REPLICA="15722"
+PGPORT_REPLICA="15721"
 PGDIR_REPLICA="/home/wanshenl/postgres"
 
 HOST_BENCHBASE=${HOST_PRIMARY}
@@ -23,12 +23,23 @@ CFGDIR_BENCHBASE="/home/wanshenl/config/benchbase/postgres"
 
 # Simple quick bash function rule: all vars used in a function must be declared at the start.
 
+# Kill any leftover Docker instances.
+function _kill_docker() {
+  USER=$1
+  HOST=$2
+
+  ssh ${USER}@${HOST} "
+    set -e
+    docker kill \$(docker ps -q)
+  "
+}
+
 # Bring up the primary.
 function _primary_up() {
   FILENAME=$1
 
-  USER=${PGUSER_PRIMARY}
-  HOST=${PGHOST_PRIMARY}
+  USER=${USER_PRIMARY}
+  HOST=${HOST_PRIMARY}
   PGDIR=${PGDIR_PRIMARY}
 
   ssh ${USER}@${HOST} "
@@ -43,8 +54,8 @@ function _primary_up() {
 function _primary_down() {
   FILENAME=$1
 
-  USER=${PGUSER_PRIMARY}
-  HOST=${PGHOST_PRIMARY}
+  USER=${USER_PRIMARY}
+  HOST=${HOST_PRIMARY}
   PGDIR=${PGDIR_PRIMARY}
 
   _kill_pid "${USER}" "${HOST}" "${PGDIR}" "${FILENAME}"
@@ -60,8 +71,8 @@ function _primary_down() {
 function _replica_up() {
   FILENAME=$1
 
-  USER=${PGUSER_REPLICA}
-  HOST=${PGHOST_REPLICA}
+  USER=${USER_REPLICA}
+  HOST=${HOST_REPLICA}
   PGDIR=${PGDIR_REPLICA}
 
   ssh ${USER}@${HOST} "
@@ -76,9 +87,9 @@ function _replica_up() {
 function _replica_down() {
   FILENAME=$1
 
-  USER=${PGUSER_PRIMARY}
-  HOST=${PGHOST_PRIMARY}
-  PGDIR=${PGDIR_PRIMARY}
+  USER=${USER_REPLICA}
+  HOST=${HOST_REPLICA}
+  PGDIR=${PGDIR_REPLICA}
 
   _kill_pid "${USER}" "${HOST}" "${PGDIR}" "${FILENAME}"
 
@@ -95,11 +106,12 @@ function _setup_docker_monitoring() {
   HOST=$2         # ssh host.
   PGDIR=$3        # Directory of postgres.
   FILENAME=$4     # Name of file for .txt, .out, .pid.
+  DOCKER_CONTAINER=$5  # Name of docker container to monitor.
 
   ssh ${USER}@${HOST} "
     set -e
     cd ${PGDIR}
-    ./cmudb/experiments/capture_docker_stats.sh primary ${FILENAME}.txt > ${FILENAME}.out 2>&1 &
+    ./cmudb/experiments/capture_docker_stats.sh ${DOCKER_CONTAINER} ${FILENAME}.txt > ${FILENAME}.out 2>&1 &
     echo \$! > ${FILENAME}.pid
   "
 }
@@ -114,9 +126,11 @@ function _setup_replication_monitoring() {
   PGUSER=${PGUSER_PRIMARY}
   PGPASS=${PGPASS_PRIMARY}
   PGPORT=${PGPORT_PRIMARY}
+  PGDIR=${PGDIR_PRIMARY}
 
   ssh ${USER}@${HOST} "
     set -e
+    cd ${PGDIR}
     echo 'Timestamp|Replay Lag (microseconds)' > ${FILENAME}.txt
     while true
     do
@@ -161,6 +175,8 @@ function _create_fn_warm_all() {
 
   ssh ${USER}@${HOST} "
     set -e
+    PGPASSWORD=${PGPASS} psql -h localhost -p ${PGPORT} -U ${PGUSER} -c 'drop extension if exists pg_prewarm;'
+    PGPASSWORD=${PGPASS} psql -h localhost -p ${PGPORT} -U ${PGUSER} -c 'create extension pg_prewarm;'
     PGPASSWORD=${PGPASS} psql -h localhost -p ${PGPORT} -U ${PGUSER} -c \"create or replace function warm_all() returns table (name varchar, type varchar, pg_prewarm_val integer) language plpgsql as \\\$func\$ begin return query select cast(concat('public.', table_name) as varchar), 'table'::varchar, cast(pg_prewarm(concat('public.', table_name)) as integer) from information_schema.tables where table_schema = 'public' union all select cast(concat('public.', indexname) as varchar), 'index'::varchar, cast(pg_prewarm(concat('public.', indexname)) as integer) from pg_indexes where schemaname = 'public'; end \\\$func\$;\"
   "
 }
@@ -239,8 +255,16 @@ function _main() {
 
   BENCHMARKS=("smallbank" "tatp" "tpcc" "ycsb")
 
+  set -x
+
   for BENCHMARK in ${BENCHMARKS[@]}
   do
+    set +e
+    _kill_docker "${USER_PRIMARY}" ${HOST_PRIMARY}
+    _kill_docker "${USER_REPLICA}" ${HOST_REPLICA}
+    set -e
+    sleep 30
+
     # Bring up the primary and replica.
     _primary_up "${BENCHMARK}_docker_compose"
     sleep 30
@@ -248,8 +272,8 @@ function _main() {
     sleep 30
 
     # Create the warm_all() function which invokes pg_prewarm on all tables and indexes.
+    # This cannot be created on the replica because the replica is in read-only mode.
     _create_fn_warm_all "${USER_PRIMARY}" "${HOST_PRIMARY}" "${PGUSER_PRIMARY}" "${PGPASS_PRIMARY}" "${PGPORT_PRIMARY}"
-    _create_fn_warm_all "${USER_REPLICA}" "${HOST_REPLICA}" "${PGUSER_REPLICA}" "${PGPASS_REPLICA}" "${PGPORT_REPLICA}"
 
     # Create and load the benchmark.
     _benchbase "${BENCHMARK}" "${BENCHMARK}_create" "--create=true"
@@ -261,25 +285,27 @@ function _main() {
 
     # Warm the caches.
     _run_primary 'SELECT * FROM warm_all();'
-    _run_replica 'SELECT * FROM warm_all();'
+    _replica_wait_sync
 
     # Collect Docker metrics on both primary and replica.
-    _setup_docker_monitoring "${USER_PRIMARY}" "${HOST_PRIMARY}" "${PGDIR_PRIMARY}" "docker_monitoring_${BENCHMARK}"
-    _setup_docker_monitoring "${USER_REPLICA}" "${HOST_REPLICA}" "${PGDIR_REPLICA}" "docker_monitoring_${BENCHMARK}"
+    _setup_docker_monitoring "${USER_PRIMARY}" "${HOST_PRIMARY}" "${PGDIR_PRIMARY}" "${BENCHMARK}_docker_monitoring" "primary"
+    _setup_docker_monitoring "${USER_REPLICA}" "${HOST_REPLICA}" "${PGDIR_REPLICA}" "${BENCHMARK}_docker_monitoring" "replica"
 
     # Collect replication lag.
-    _setup_replication_monitoring "replication_lag_${BENCHMARK}"
+    _setup_replication_monitoring "${BENCHMARK}_replication_lag"
 
     # Execute the workload.
     _benchbase "${BENCHMARK}" "${BENCHMARK}_execute" "--execute=true"
 
+    set +e
     # Stop collecting replication lag.
-    _kill_pid "replication_lag_${BENCHMARK}"
+    _kill_pid "${USER_PRIMARY}" "${HOST_PRIMARY}" "${PGDIR_PRIMARY}" "${BENCHMARK}_replication_lag"
     # Stop collecting Docker metrics.
-    _kill_pid "${USER_PRIMARY}" "${HOST_PRIMARY}" "${PGDIR_PRIMARY}" "docker_monitoring_${BENCHMARK}"
-    _kill_pid "${USER_REPLICA}" "${HOST_REPLICA}" "${PGDIR_REPLICA}" "docker_monitoring_${BENCHMARK}"
     _force_kill_docker_metrics "${USER_PRIMARY}" "${HOST_PRIMARY}"
     _force_kill_docker_metrics"${USER_REPLICA}" "${HOST_REPLICA}"
+    _kill_pid "${USER_PRIMARY}" "${HOST_PRIMARY}" "${PGDIR_PRIMARY}" "${BENCHMARK}_docker_monitoring"
+    _kill_pid "${USER_REPLICA}" "${HOST_REPLICA}" "${PGDIR_REPLICA}" "${BENCHMARK}_docker_monitoring"
+    set -e
 
     # Bring down the primary and replica.
     _replica_down "${BENCHMARK}_docker_compose"
