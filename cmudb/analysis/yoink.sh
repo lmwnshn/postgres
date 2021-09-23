a81d319887065b8fe9f3de4df65150c9dc2eb964
Wan Shen Lim <wanshen.lim@gmail.com>
2021-09-23 10:57:08 -0400

    Checkpoint.

--- cmudb/analysis/yoink.sh
+++ cmudb/analysis/yoink.sh
@@ -4,9 +4,12 @@ BENCHMARKS=("smallbank" "tatp" "tpcc" "ycsb")
 
 for BENCHMARK in "${BENCHMARKS[@]}"
 do
-  scp dev10:~/postgres/${BENCHMARK}_docker_compose.out .
-  scp dev10:~/postgres/${BENCHMARK}_docker_monitoring.out .
-  scp dev10:~/postgres/${BENCHMARK}_docker_monitoring.txt .
+  scp dev6:~/postgres/${BENCHMARK}_docker_compose.out ./${BENCHMARK}_docker_compose_replica.out
+  scp dev6:~/postgres/${BENCHMARK}_docker_monitoring.out ./${BENCHMARK}_docker_monitoring_replica.out
+  scp dev6:~/postgres/${BENCHMARK}_docker_monitoring.txt ./${BENCHMARK}_docker_monitoring_replica.txt
+  scp dev10:~/postgres/${BENCHMARK}_docker_compose.out ./${BENCHMARK}_docker_compose_primary.out
+  scp dev10:~/postgres/${BENCHMARK}_docker_monitoring.out ./${BENCHMARK}_docker_monitoring_primary.out
+  scp dev10:~/postgres/${BENCHMARK}_docker_monitoring.txt ./${BENCHMARK}_docker_monitoring_primary.txt
   scp dev10:~/postgres/${BENCHMARK}_replication_lag.out .
   scp dev10:~/postgres/${BENCHMARK}_replication_lag.txt .
   scp dev10:~/benchbase/target/benchbase-2021-SNAPSHOT/${BENCHMARK}_create.out .
